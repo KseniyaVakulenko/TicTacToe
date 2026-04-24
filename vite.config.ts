eda7c821df2c794e35e0bb580696ab190ca32ae4
Kseniya <89528940+KseniyaVakulenko@users.noreply.github.com>
2026-04-24 06:00:32 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/TicTacToe/', // ← замени на имя своего репозитория
   plugins: [react(), tailwindcss()],
 })
