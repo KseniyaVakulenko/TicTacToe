cb38756ac273555a2820d1261450110db08dab7c
Kseniya <89528940+KseniyaVakulenko@users.noreply.github.com>
2026-04-24 06:51:00 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // или @vitejs/plugin-react
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/TicTacToe/', // ← замени на имя своего репозитория
+  // Замени <repo-name> на название твоего репозитория
+  base: '/<TicTacToe>/',
   plugins: [react(), tailwindcss()],
 })
